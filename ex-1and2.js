919f2d406594725516405568400846a454d1f7ca
NoonMira <163841273+NoonMira@users.noreply.github.com>
2024-05-09 12:44:43 +0700

    feat: add Assignment

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,14 @@
-//Exercise #1: Variable Creation
-//Start coding here
+let fristName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
 
-//Exercise #2: Variable Reassignment
-//Start coding here
+const restaurantName = "Chef's Table by Chef James";
+const totalPrice = 15000;
+const openingRestaurantName = 1500;
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalPrice = 5000;
+openingRestaurantName = 2500;
+
+
+
